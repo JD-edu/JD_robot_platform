3a7de996948e4c43e1a09474a6d4e300ad1fa4f6
JD-edu <96219601+JD-edu@users.noreply.github.com>
2023-12-11 21:25:52 +0900

    Dec 11th

--- 106_7_get_pitch_roll_yaw_gyro_mag/106_7_get_pitch_roll_yaw_gyro_mag.ino
+++ 106_7_get_pitch_roll_yaw_gyro_mag/106_7_get_pitch_roll_yaw_gyro_mag.ino
@@ -8,7 +8,7 @@ void setup() {
     Wire.begin();
     delay(2000);
 
-    if (!mpu.setup(0x68)) {  // change to your own address
+    if (!mpu.setup(0x69)) {  // change to your own address
         while (1) {
             Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
             delay(5000);
