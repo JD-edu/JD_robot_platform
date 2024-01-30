56b1fef7ec1b1bfa07e6983c3b3f066160d77c0f
JD-edu <96219601+JD-edu@users.noreply.github.com>
2024-01-30 14:53:21 +0900

    Update 207_tb6612_custom_motor_drive.ino

--- 207_tb6612_custom_motor_drive/207_tb6612_custom_motor_drive.ino
+++ 207_tb6612_custom_motor_drive/207_tb6612_custom_motor_drive.ino
@@ -17,55 +17,109 @@
 #define DEFAULT_LEDC_FREQ 20000
 #define DEFAULT_LEDC_RANGE LEDC_TIMER_10_BIT
 
-void motor1_setup(int ia1, int ib1, int pwm1){
+void motor1_begin(int ia1, int ib1, int pwm1){
   pinMode(ia1, OUTPUT);
   pinMode(ib1, OUTPUT);
   ledcSetup(LEDC_CHANNEL_0, DEFAULT_LEDC_FREQ, LEDC_TIMER_10_BIT);
   ledcAttachPin(pwm1, LEDC_CHANNEL_0 );
 }
 
-void motor2_setup(int ia2, int ib2, int pwm2 ){
+void motor2_begin(int ia2, int ib2, int pwm2 ){
   pinMode(ia2, OUTPUT);
   pinMode(ib2, OUTPUT);
   ledcSetup(LEDC_CHANNEL_0, DEFAULT_LEDC_FREQ, DEFAULT_LEDC_RANGE);
-  ledcAttachPin(LEDC_CHANNEL_0, pwm2);
+  ledcAttachPin(pwm2, LEDC_CHANNEL_0);
 }
 
-void motor3_setup(int ia3, int ib3, int pwm3 ){
+void motor3_begin(int ia3, int ib3, int pwm3 ){
   pinMode(ia3, OUTPUT);
   pinMode(ib3, OUTPUT);
   ledcSetup(LEDC_CHANNEL_0, DEFAULT_LEDC_FREQ, DEFAULT_LEDC_RANGE);
   ledcAttachPin(LEDC_CHANNEL_0, pwm3);
 }
 
-void motor4_setup(int ia4, int ib4, int pwm4 ){
+void motor4_begin(int ia4, int ib4, int pwm4 ){
   pinMode(ia4, OUTPUT);
   pinMode(ib4, OUTPUT);
   ledcSetup(LEDC_CHANNEL_0, DEFAULT_LEDC_FREQ, DEFAULT_LEDC_RANGE);
   ledcAttachPin(LEDC_CHANNEL_0, pwm4);
 }
 
-void tb6612_enable(int m_sleep){
+void motor_enable(int m_sleep){
   pinMode(m_sleep, OUTPUT);
   digitalWrite(m_sleep, HIGH);
 }
 
-void motor1_move_forward(){
+void motor1_move_forward(int speed){
+  if(speed < 0)
+    speed = 0;
+  if(speed > 1024)
+    speed = 1024;
   digitalWrite(IA1, HIGH);
   digitalWrite(IB1, LOW);
-  ledcWrite(LEDC_CHANNEL_0, 1024);
+  ledcWrite(LEDC_CHANNEL_0, speed);
+}
+
+void motor2_move_forward(int speed){
+  if(speed < 0)
+    speed = 0;
+  if(speed > 1024)
+    speed = 1024;
+  digitalWrite(IA2, HIGH);
+  digitalWrite(IB2, LOW);
+  ledcWrite(LEDC_CHANNEL_0, speed);
+}
+
+void motor1_move_backward(int speed){
+  if(speed < 0)
+    speed = 0;
+  if(speed > 1024)
+    speed = 1024;
+  digitalWrite(IA1, LOW);
+  digitalWrite(IB1, HIGH);
+  ledcWrite(LEDC_CHANNEL_0, speed);
+}
+
+void motor2_move_backward(int speed){
+  if(speed < 0)
+    speed = 0;
+  if(speed > 1024)
+    speed = 1024;
+  digitalWrite(IA2, LOW);
+  digitalWrite(IB2, HIGH);
+  ledcWrite(LEDC_CHANNEL_0, speed);
+}
+
+void motor1_stop(){
+  digitalWrite(IA1, LOW);
+  digitalWrite(IB1, LOW);
+  ledcWrite(LEDC_CHANNEL_0, 0);
+}
+
+void motor2_stop(){
+  digitalWrite(IA2, LOW);
+  digitalWrite(IB2, LOW);
+  ledcWrite(LEDC_CHANNEL_0, 0);
 }
 
 void setup() {
   Serial.begin(115200);
-  motor1_setup(IA1, IB1, PWM1);
-  //motor2_setup(IA1, IB1, PWM2);
+  motor1_begin(IA1, IB1, PWM1);
+  motor2_begin(IA2, IB2, PWM2);
   //motor3_setup(IA1, IB1, PWM3);
   //motor4_setup(IA1, IB1, PWM4);
-  tb6612_enable(MSLEEP);
+  motor_enable(MSLEEP);
   delay(1000);
 }
 
 void loop() {
- motor1_move_forward();
+  motor1_move_forward(500);
+  motor2_move_forward(500);
+  delay(1000);
+  motor1_stop();
+  motor2_stop();
+  delay(1000);
+  motor1_move_backward(500);
+  motor2_move_backward(500);
+  delay(1000);
 }
